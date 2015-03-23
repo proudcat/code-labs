80e641543b09853c01396375593fb25c4d07e551
于小懒 <koalaylj@gmail.com>
2015-03-23 18:06:43 +0800

    add  simple json demo

---
+++ csharp/Demo.Json/Program.cs
@@ -0,0 +1,30 @@
+﻿using SimpleJson;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Json
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string json = File.ReadAllText("./rebirthConfig.json");
+
+           // List<Rebirth> obj_list = SimpleJson.SimpleJson.DeserializeObject<List<Rebirth>>(json);
+            
+            
+            
+            Rebirth[] obj_array = SimpleJson.SimpleJson.DeserializeObject<Rebirth[]>(json);
+
+
+
+            Console.WriteLine(obj_array[0]._id + "," + obj_array[0].moneyCost[0]);
+            Console.ReadKey();
+        }
+    }
+}
