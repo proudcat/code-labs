fb7228ee833ee71066a6032b618eca7eab5ad535
于小懒 <koalaylj@gmail.com>
2016-03-01 10:54:34 +0900

    this dmeo

--- nodejs/this.js
+++ nodejs/this.js
@@ -1,56 +1,76 @@
-var something, another;
+var another;
 
-something = {
+var Chanel = (function () {
 
-  x : 'x',
+  var reqLogin = function (msg, cb, context) {
 
-  print : function( callback ){
-    callback && callback.call( this );
-    console.log( this.x );
-  }
+    console.log("requset login in......");
+
+    context = context || this;
+
+    setTimeout(function () {
+
+      if (typeof cb === "function") {
+        var dto = {
+          userMoney: 1000
+        };
+        // cb(dto);
+        cb.call(context, dto);
+      } else {
+        console.warn("message does not have a callback function");
+      }
+
+    }, 1500);
+  };
+
+  return {
+    reqLogin: reqLogin
+  };
+})();
+
+
+var logic = function () {
+
+  this.bet = 10;
+
+  this.login = function () {
+
+    var msg = {
+      userName: "yulijun",
+      password: "123"
+    };
+
+    Chanel.reqLogin(msg, function (dto) {
+      console.log("start game.........");
+      console.log(this.bet, dto.userMoney);
+    },this);
+  };
 };
 
-another = {
+var shit = new logic();
+shit.login();
 
-  x : 'a',
 
-  set_x : function(){
-    this.x = 'b';
-  }
-};
-
-// situation a
-something.print( function(){
-  another.set_x();
-});
-
-// situation b
-something.print( another.set_x );
-
-// situation c
-something.print( function(){
-  another.set_x.call( this );
-});
 
 
 var example = {
-  
-  name : 'who',
-  
-  wrong : function(){
-    setTimeout( function(){
-      console.log( this.name );
-    }, 0 );
+
+  name: 'who',
+
+  wrong: function () {
+    setTimeout(function () {
+      console.log(this.name);
+    }, 0);
   },
-  
-  right : function(){
+
+  right: function () {
     var self = this;
-    
-    setTimeout( function(){
-      console.log( self.name );
-    }, 0 );
+
+    setTimeout(function () {
+      console.log(self.name);
+    }, 0);
   }
 };
 
-example.wrong();
-example.right();
+// example.wrong();
+// example.right();
